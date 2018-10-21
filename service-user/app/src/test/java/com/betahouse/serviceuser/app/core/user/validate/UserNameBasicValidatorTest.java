f5f04470ff7799d1b0d3b755aa7b23d72bab09a5
dango.yxm <yinxiaoming96@gmail.com>
2018-10-21 23:33:15 +0800

    1.引入mockito进行用例覆盖

--- service-user/app/src/test/java/com/betahouse/serviceuser/app/core/user/validate/UserNameBasicValidatorTest.java
+++ service-user/app/src/test/java/com/betahouse/serviceuser/app/core/user/validate/UserNameBasicValidatorTest.java
@@ -1,14 +1,22 @@
 package com.betahouse.serviceuser.app.core.user.validate;
 
+import com.betahouse.serviceuser.app.common.dao.model.UserDO;
+import com.betahouse.serviceuser.app.common.dao.repo.UserRepo;
 import com.betahouse.serviceuser.app.core.user.model.UserBO;
 import exceptions.BetahouseException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 import utils.AssertUtil;
 import utils.TestAssertUtil;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * 用户名基础验证器测试实例
@@ -22,6 +30,14 @@ public class UserNameBasicValidatorTest {
 
     @Autowired
     private UserNameBasicValidator userNameBasicValidator;
+
+    private UserRepo mockUserRepo;
+
+    @Before
+    public void before(){
+        mockUserRepo = mock(UserRepo.class);
+        ReflectionTestUtils.setField(userNameBasicValidator, "userRepo", mockUserRepo);
+    }
 
     @Test
     public void support() {
@@ -48,7 +64,16 @@ public class UserNameBasicValidatorTest {
             userNameBasicValidator.validate(user);
         });
 
+        // 账号已存在
+        TestAssertUtil.assertThrow(BetahouseException.class, () -> {
+            UserBO user = new UserBO();
+            user.setUsername("asas");
+            when(mockUserRepo.getByUsername(anyString())).thenReturn(new UserDO());
+            userNameBasicValidator.validate(user);
+        });
+
         // 账号正常
+        when(mockUserRepo.getByUsername(anyString())).thenReturn(null);
         UserBO user = new UserBO();
         user.setUsername("ssasasa");
         userNameBasicValidator.validate(user);
